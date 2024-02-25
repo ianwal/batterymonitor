ab0f2e13400fb90ff0def5fd5a6265f838a41b44
ianwal <52143079+ianwal@users.noreply.github.com>
2024-02-25 14:09:25 -0800

    Fix crashes in ADC battery after cleanup

--- board/src/battery.cpp
+++ board/src/battery.cpp
@@ -19,54 +19,33 @@ constexpr auto BATT_VOLTAGE_ADC_ATTEN{ADC_ATTEN_DB_11};
 constexpr auto BATT_VOLTAGE_ADC_BITWIDTH{ADC_BITWIDTH_12};
 constexpr auto ADC_UNIT{ADC_UNIT_1};
 
-// Calibrate the battery voltage measurement based on the resistor voltage divider.
-constexpr float const VDIV_RATIO{5.02f};
+adc_oneshot_unit_handle_t batt_voltage_adc_handle{};
+adc_cali_handle_t batt_voltage_adc_cali_handle{};
 
-adc_oneshot_unit_handle_t batt_voltage_adc_handle{nullptr};
-adc_cali_handle_t batt_voltage_adc_cali_handle{nullptr};
-bool is_adc_calibrated{false};
-
-bool adc_calibration_init(adc_unit_t unit, adc_channel_t channel, adc_atten_t atten, adc_bitwidth_t bitwidth,
+void adc_calibration_init(adc_unit_t unit, adc_channel_t channel, adc_atten_t atten, adc_bitwidth_t bitwidth,
                           adc_cali_handle_t *out_handle)
 {
-        adc_cali_handle_t handle{nullptr};
-        auto ret = static_cast<esp_err_t>(ESP_FAIL);
-
-        if (!is_adc_calibrated) {
 #if ADC_CALI_SCHEME_CURVE_FITTING_SUPPORTED
                 adc_cali_curve_fitting_config_t cali_config{};
                 cali_config.unit_id = unit;
                 cali_config.chan = channel;
                 cali_config.atten = atten;
                 cali_config.bitwidth = bitwidth;
-                ret = adc_cali_create_scheme_curve_fitting(&cali_config, &handle);
+                ESP_ERROR_CHECK(adc_cali_create_scheme_line_fitting(&cali_config, &handle));
 
 #elif ADC_CALI_SCHEME_LINE_FITTING_SUPPORTED
-                adc_cali_line_fitting_config_t cali_config{};
-                cali_config.unit_id = unit;
-                cali_config.atten = atten;
-                cali_config.bitwidth = bitwidth;
-                ret = adc_cali_create_scheme_line_fitting(&cali_config, &handle);
+        adc_cali_line_fitting_config_t cali_config;
+        cali_config.unit_id = unit;
+        cali_config.atten = atten;
+        cali_config.bitwidth = bitwidth;
+        ESP_ERROR_CHECK(adc_cali_create_scheme_line_fitting(&cali_config, out_handle));
 #else
 #error "No ADC calibration scheme defined"
 #endif
-                is_adc_calibrated = (ret == ESP_OK);
-        }
-
-        *out_handle = handle;
-        if (ret == ESP_OK) {
-                ESP_LOGI(TAG, "Calibration Success");
-        } else if (ret == ESP_ERR_NOT_SUPPORTED || !is_adc_calibrated) {
-                ESP_LOGW(TAG, "eFuse not burnt, skip software calibration");
-        } else {
-                ESP_LOGE(TAG, "Invalid arg or no memory");
-        }
-
-        return is_adc_calibrated;
 }
 
 // Deinitialize calibration of an ADC handle
-void adc_calibration_deinit(adc_cali_handle_t handle)
+void adc_calibration_deinit(adc_cali_handle_t &handle)
 {
 #if ADC_CALI_SCHEME_CURVE_FITTING_SUPPORTED
         ESP_ERROR_CHECK_WITHOUT_ABORT(adc_cali_delete_scheme_curve_fitting(handle));
@@ -78,7 +57,7 @@ void adc_calibration_deinit(adc_cali_handle_t handle)
 }
 
 // Configures the battery ADC.
-void config_battery_adc(adc_oneshot_unit_handle_t adc_handle)
+void config_battery_adc(adc_oneshot_unit_handle_t &adc_handle)
 {
         // ADC1 Init
         adc_oneshot_unit_init_cfg_t init_config{};
@@ -100,25 +79,22 @@ float get_battery_voltage()
         // ADC1 Read
         int raw_adc_reading{};
         ESP_ERROR_CHECK(adc_oneshot_read(batt_voltage_adc_handle, BATT_VOLTAGE_ADC_CHANNEL, &raw_adc_reading));
-        ESP_LOGI(TAG, "ADC%d Channel[%d] Raw Data: %d", ADC_UNIT + 1, BATT_VOLTAGE_ADC_CHANNEL, raw_adc_reading);
+        // ESP_LOGI(TAG, "ADC%d Channel[%d] Raw Data: %d", ADC_UNIT + 1, BATT_VOLTAGE_ADC_CHANNEL, raw_adc_reading);
 
         // ADC1 Calibration
-        bool const do_calibration_batt_voltage{adc_calibration_init(ADC_UNIT, BATT_VOLTAGE_ADC_CHANNEL,
-                                                                    BATT_VOLTAGE_ADC_ATTEN, BATT_VOLTAGE_ADC_BITWIDTH,
-                                                                    &batt_voltage_adc_cali_handle)};
+        adc_calibration_init(ADC_UNIT, BATT_VOLTAGE_ADC_CHANNEL, BATT_VOLTAGE_ADC_ATTEN, BATT_VOLTAGE_ADC_BITWIDTH,
+                             &batt_voltage_adc_cali_handle);
 
         int voltage{};
-        if (do_calibration_batt_voltage) {
-                ESP_ERROR_CHECK(adc_cali_raw_to_voltage(batt_voltage_adc_cali_handle, raw_adc_reading, &voltage));
-                ESP_LOGI(TAG, "ADC%d Channel[%d] Cali Voltage: %d mV", ADC_UNIT + 1, BATT_VOLTAGE_ADC_CHANNEL, voltage);
-        }
+        ESP_ERROR_CHECK(adc_cali_raw_to_voltage(batt_voltage_adc_cali_handle, raw_adc_reading, &voltage));
+        ESP_LOGI(TAG, "ADC%d Channel[%d] Cali Voltage: %d mV", ADC_UNIT + 1, BATT_VOLTAGE_ADC_CHANNEL, voltage);
 
         // ADC1 Teardown
         ESP_ERROR_CHECK(adc_oneshot_del_unit(batt_voltage_adc_handle));
-        if (do_calibration_batt_voltage) {
-                adc_calibration_deinit(batt_voltage_adc_cali_handle);
-        }
+        adc_calibration_deinit(batt_voltage_adc_cali_handle);
 
+        // Calibrate the battery voltage measurement based on the resistor voltage divider.
+        constexpr float const VDIV_RATIO{5.02f};
         return (voltage / 1000.f) * VDIV_RATIO;
 }
 
@@ -137,13 +113,13 @@ std::unique_ptr<HAEntity> create_battery_entity()
                 return std::string_view(voltage_char_buffer.data(), res.ptr);
         }();
         */
-        auto const battery_voltage_string = [] {
+        auto battery_voltage_string = [] {
                 auto const voltage = get_battery_voltage();
                 auto result = std::to_string(voltage);
-                result.resize(8);
+                result.resize(8); // Truncate decimals, assuming left of the decimal point is less than 7 digits.
                 return result;
         }();
-        entity->state = battery_voltage_string;
+        entity->state = std::move(battery_voltage_string);
         entity->entity_id = "sensor.car_battery";
         entity->add_attribute("friendly_name", "Car Battery Voltage");
         entity->add_attribute("unit_of_measurement", "Volts");
